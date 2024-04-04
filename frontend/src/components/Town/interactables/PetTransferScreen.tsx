0f23e43e0a8b6dbf1b6d36dee42409924280f967
Will Feldman <13539982+willfeldman@users.noreply.github.com>
2024-04-03 20:47:51 -0400

    Only render pets after transfer pet selection

--- frontend/src/components/Town/interactables/PetTransferScreen.tsx
+++ frontend/src/components/Town/interactables/PetTransferScreen.tsx
@@ -20,7 +20,6 @@ import PlayerPets from '../../SocialSidebar/PlayerPets';
 export default function PetTransferScreen(): JSX.Element {
   const players = usePlayers();
   const { ourPlayer } = useTownController();
-  //const transferTo = players.filter(player => player.id !== ourPlayer.id).concat([]); WHILE IN DEVELOPMENT USE BELOW BECAUSE THERE"S ONLY ONE PLAYER
   const transferTo = players
     .concat([])
     .sort((p1, p2) =>
@@ -35,7 +34,6 @@ export default function PetTransferScreen(): JSX.Element {
   }, [ourPlayer.pets]);
 
   function handleTransfer(playerID: string) {
-    //add pet to selected player
     const selectedPlayer = players.find(player => player.id === playerID);
     const petToTransfer = ourPlayer.pets?.find(pet => pet.id === petToTransferID);
     if (selectedPlayer && petToTransfer) {
@@ -43,8 +41,16 @@ export default function PetTransferScreen(): JSX.Element {
       ourPlayer.removePet(petToTransferID);
       toast({
         title: 'Pet transferred successfully.',
-        description: `Pet has been transferred to ${selectedPlayer.userName}.`,
+        description: `Pet ${petToTransfer.id} has been transferred to ${selectedPlayer.userName}.`,
         status: 'success',
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: 'Pet transfer failed.',
+        description: 'Please try again.',
+        status: 'error',
         duration: 5000,
         isClosable: true,
       });
@@ -67,34 +73,36 @@ export default function PetTransferScreen(): JSX.Element {
           </option>
         ))}
       </Select>
-      <OrderedList spacing={3}>
-        {transferTo.map(player => (
-          <>
-            <ListItem key={player.id} d='flex' alignItems='center' justifyContent='space-between'>
-              <Stack direction='row' align='center'>
-                <VStack align='start'>
-                  <Text fontWeight='bold'>
-                    <PlayerName player={player} />
-                  </Text>
-                  <Text>
-                    <PlayerPets player={player} />
-                  </Text>
-                </VStack>
-              </Stack>
-              <Button
-                colorScheme='teal'
-                variant='outline'
-                disabled={player.id === ourPlayer.id}
-                onClick={() => {
-                  handleTransfer(player.id);
-                }}>
-                Transfer to {player.userName}
-              </Button>
-            </ListItem>
-            <hr />
-          </>
-        ))}
-      </OrderedList>
+      {petToTransferID && ( // Conditional rendering based on petToTransferID
+        <OrderedList spacing={3}>
+          {transferTo.map(player => (
+            <>
+              <ListItem key={player.id} d='flex' alignItems='center' justifyContent='space-between'>
+                <Stack direction='row' align='center'>
+                  <VStack align='start'>
+                    <Text fontWeight='bold'>
+                      <PlayerName player={player} />
+                    </Text>
+                    <Text>
+                      <PlayerPets player={player} />
+                    </Text>
+                  </VStack>
+                </Stack>
+                <Button
+                  colorScheme='teal'
+                  variant='outline'
+                  disabled={player.id === ourPlayer.id}
+                  onClick={() => {
+                    handleTransfer(player.id);
+                  }}>
+                  Transfer to {player.userName}
+                </Button>
+              </ListItem>
+              <hr />
+            </>
+          ))}
+        </OrderedList>
+      )}
     </Box>
   );
 }

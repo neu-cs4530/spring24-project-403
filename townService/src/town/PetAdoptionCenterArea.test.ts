22fe0c8a005fba60dc55a7e772d32a1ed334315a
Bambi <bzhuangg@gmail.com>
2024-04-07 20:54:06 -0400

    PetAdoptionCenterArea tests (preliminary)

--- townService/src/town/PetAdoptionCenterArea.test.ts
+++ townService/src/town/PetAdoptionCenterArea.test.ts
@@ -19,5 +19,57 @@ describe('PetAdoptionCenterArea', () => {
     testArea.add(newPlayer);
   });
 
-  describe('testing testing', () => {});
+  describe('add', () => {
+    it('Adds the player to the occupants list and emits an interactableUpdate event', () => {
+      expect(testArea.occupantsByID).toEqual([newPlayer.id]);
+
+      const lastEmittedUpdate = getLastEmittedEvent(townEmitter, 'interactableUpdate');
+      expect(lastEmittedUpdate).toEqual({
+        pets: [],
+        id,
+        occupants: [newPlayer.id],
+        type: 'PetAdoptionCenter',
+      });
+    });
+  });
+  describe('remove', () => {
+    it('Removes the player from the list of occupants and emits an interactableUpdate event', () => {
+      const extraPlayer = new Player(nanoid(), mock<TownEmitter>());
+      testArea.add(extraPlayer);
+      testArea.remove(newPlayer);
+
+      expect(testArea.occupantsByID).toEqual([extraPlayer.id]);
+      const lastEmittedUpdate = getLastEmittedEvent(townEmitter, 'interactableUpdate');
+      expect(lastEmittedUpdate).toEqual({
+        pets: [],
+        id,
+        occupants: [extraPlayer.id],
+        type: 'PetAdoptionCenter',
+      });
+    });
+  });
+  describe('fromMapObject', () => {
+    it('Throws an error if the width or height are missing', () => {
+      expect(() =>
+        PetAdoptionCenter.fromMapObject(
+          { id: 1, name: nanoid(), visible: true, x: 0, y: 0 },
+          townEmitter,
+        ),
+      ).toThrowError();
+    });
+    it('Creates a new pet adoption center area using the provided boundingBox and id, with an empty occupants list', () => {
+      const x = 30;
+      const y = 20;
+      const width = 10;
+      const height = 20;
+      const name = 'name';
+      const val = PetAdoptionCenter.fromMapObject(
+        { x, y, width, height, name, id: 10, visible: true },
+        townEmitter,
+      );
+      expect(val.boundingBox).toEqual({ x, y, width, height });
+      expect(val.id).toEqual(name);
+      expect(val.occupantsByID).toEqual([]);
+    });
+  });
 });
